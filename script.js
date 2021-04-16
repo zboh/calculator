ab0550597f97dfd6a86883005edcd479a2c2397a
Herman Sabo <szabo.herman@gmail.com>
2021-04-17 00:10:08 +0400

    adds basic html and icon.

---
+++ script.js
@@ -0,0 +1,21 @@
+// functions of math operations
+
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
+
+function operate(operator, a, b) {
+	return operator(a, b);
+}
